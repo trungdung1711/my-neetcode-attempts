673513d9dffb62abdaeab71111cf641a6adc90c0
Le Bui Trung Dung <trungdunglebui17112004@gmail.com>
2026-04-26 08:52:24 +0900

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        # input: array of numbers
+        # output: true if any value appears at least twice
+        # false if distinct
+
+        # constraint
+        # len(nums) >= 1
+
+        # edge case
+        # [3] -> true
+
+        # sol2
+        
+
+
+        # sol1
+        dup : set = set()
+
+        for num in nums:
+            if num in dup:
+                return True
+            
+            dup.add(num)
+        
+        return False
+        

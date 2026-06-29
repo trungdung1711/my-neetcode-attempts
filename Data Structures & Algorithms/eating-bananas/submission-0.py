931381128daac76a83c78fba2987a6687ba9f93d
Dung Le <trungdunglebui17112004@gmail.com>
2026-06-29 07:44:22 +0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # input
+        # n piles of bananas
+        # the guard will come back in h hours
+        # koko can choose her eating speed
+        # if k = 5, but the pile has only 4
+        # koko will eat all, and stop in that hour
+
+        # output
+        # minimun integer k
+        # such that in h hours
+        # koko can eat all the banana
+
+        # constraints
+        # len(piles) >= 1 <= 10^4
+        # len(piles) <= h <= 10^9
+        # piles[i] >= 1 <= 10^9
+
+        # edge cases
+
+        def hours(k) -> int:
+            return sum([math.ceil(banana / k) for banana in piles])
+
+        left = 1
+        right = max (piles)
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            hour = hours(mid)
+
+            if hour > h:
+                left = mid + 1
+
+            elif hour < h:
+                right = mid - 1
+
+            else:
+                # not always correct
+                right =- 1
+
+        return left
